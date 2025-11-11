919dbd392fa9bc215d9da7e36645872774fd3dc2
רחלי בינט <rachelbinet@MacBook-Air-sl-rhly.local>
2025-11-11 22:07:31 +0200

    first mission

--- Algebra.java
+++ Algebra.java
@@ -9,59 +9,94 @@ public class Algebra {
 	    System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
+ 		 System.out.println(times(3,4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
    		System.out.println(div(12,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
+   		System.out.println(div(25,6));   // 25 / 7
+   		System.out.println(12%5);
+		System.out.println(mod(25,7));   // 25 % 7
+		
    		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
+   		System.out.println(sqrt(35));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
+		System.out.println(Math.sqrt(76123));
+
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
-
+    while (x2 != 0) {
+        x1++;
+        x2--;
+    }
+    return x1;
+}
+ 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    while (x2 != 0) {
+        x1--;
+        x2--;
+    }
+    return x1;
+}
 
-	// Returns x1 * x2
+ 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int timess=0;
+		while(x2>0){
+			timess = plus(timess, x1);  // add x1 to result
+        x2--;  
+		}
+		return timess;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result=1;
+		while(n>0){
+			n--;
+			result = times(result, x);
+		}
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	public static int div(int x1, int x2)
+	 {
+	int counter=0;
+	int result = x1;
+		while(result>0){
+			if(result<x2){return counter;}
+			result = minus(result, x2);  
+        	counter++;  
+		}	
+			return counter;
+
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+
+	int result = x1;
+		while(result>=x2){
+			result = minus(result, x2);  
+		}	
+			return result;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+	int num=1;
+	while(times(num,num)<x){
+		num++;
+	}
+	return(num);
+	
+	
+		}	 	  
 }
